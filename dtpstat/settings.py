b4a25c56d362bc82aaaa5b8cb6f761a2cd4a2039
ustnv <ustinovichsergey@gmail.com>
2020-05-11 00:50:46 +0300

    fix bug

--- dtpstat/settings.py
+++ dtpstat/settings.py
@@ -96,8 +96,8 @@ DATE_FORMAT = 'd.m.Y'
 CKEDITOR_UPLOAD_PATH = 'blog/'
 
 STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = env('STATIC_ROOT')
+STATICFILES_DIRS = [env('STATICFILES_DIRS')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
