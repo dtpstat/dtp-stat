11970e8916047a0f2b4b267320942271af6b416f
Ilia Shlykov <shlykov@github.com>
2021-09-14 13:56:25 +0500

    Fix opendata error

--- application/utils.py
+++ application/utils.py
@@ -90,6 +90,8 @@ def get_region_by_center_point(center_point):
 
 
 def opendata(region=None, force=False):
+    if not os.path.exists(settings.MEDIA_ROOT + "/opendata/"):
+        os.makedirs(settings.MEDIA_ROOT + "/opendata/")
     if region:
         active_region_ids = [region.id]
     else:
