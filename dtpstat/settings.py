57dc63825cc8dd81ec2e889cb290a609e9176952
Ilya Shlykov <shlykov@users.noreply.github.com>
2024-10-15 14:35:40 +0300

    Fix

--- dtpstat/settings.py
+++ dtpstat/settings.py
@@ -74,7 +74,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'application.context_processors.get_donate_data',
                 'application.context_processors.settings',
             ],
         },
