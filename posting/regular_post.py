c67ce8401e3cc8e5b513b3a2a07964ba39e4ad98
Mikhail Lebedev <last.lebedev@gmail.com>
2025-08-27 04:17:14 +0300

    Fix verbose names in RegularPost model and add string representation

--- posting/regular_post.py
+++ posting/regular_post.py
@@ -2,5 +2,8 @@ from django.db import models
 
 class RegularPost(models.Model):
     class Meta:
-        verbose_name = 'Регулярный постинг'
-        verbose_name_plural = 'Регулярный постинг'
+        verbose_name = 'Регулярный пост'
+        verbose_name_plural = 'Регулярные посты'
+
+    def __str__(self) -> str:
+        return f'Регулярный пост #{self.pk}'
