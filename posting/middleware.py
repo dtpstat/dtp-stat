9cb163d7ebf342d5e828ecd7a2eb9a475e87a9d7
Mikhail Lebedev <last.lebedev@gmail.com>
2025-08-27 04:44:26 +0300

    Refactor timezone handling in middleware to use settings for default timezone and improve error handling

--- posting/middleware.py
+++ posting/middleware.py
@@ -1,15 +1,18 @@
 from django.utils import timezone
+from django.conf import settings
 import pytz
+from pytz import UnknownTimeZoneErro
 
 class TimezoneMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        tzname = request.COOKIES.get('user_timezone', 'UTC')
-        if tzname in pytz.all_timezones:
-            timezone.activate(pytz.timezone(tzname))
-        else:
-            timezone.activate(pytz.UTC)
+        tzname = request.COOKIES.get('user_timezone', settings.TIME_ZONE)
+        try:
+            tz = ZoneInfo(tzname)
+        except Exception:
+            tz = ZoneInfo(settings.TIME_ZONE)
+        timezone.activate(tz)
         response = self.get_response(request)
         return response
