cd792c92010f568a0caaf1c380876d473db376d5
Mikhail Lebedev <last.lebedev@gmail.com>
2025-08-27 16:18:16 +0300

    Align RegularPost verbose names with initial migration

--- publisher/regular_post.py
+++ publisher/regular_post.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class RegularPost(models.Model):
     class Meta:
-        verbose_name = 'Регулярный пост'
-        verbose_name_plural = 'Регулярные посты'
+        verbose_name = 'Регулярный постинг'
+        verbose_name_plural = 'Регулярный постинг'
 
     def __str__(self) -> str:
         return f'Регулярный пост #{self.pk}'
